f90904a5b5d157fda2808d44607bc71f15fd1d5f
appuser <appuser@journey2-67f844fd46-k8chn>
2021-02-12 06:24:39 +0000

    starter project

---
+++ my_algorithm.js
@@ -0,0 +1,8 @@
+let list_with_nums = [5, 3, 1, 2, 4];
+
+// TODO: Here you should write the algorithm that your peer explain to you
+
+
+// This part tests your code
+const resultDiv = document.querySelector('.result');
+resultDiv.innerHTML = list_with_nums.join(', ');
